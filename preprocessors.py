1bd1531338a3ad1650260df32c883ae6d97355de
yunik1004@gmail.com <inkyu@DESKTOP-INKYU.localdomain>
2018-01-24 20:21:26 +0900

    Insert the comment to the functions

--- preprocessors.py
+++ preprocessors.py
@@ -3,10 +3,12 @@ import numpy as np
 from skimage.transform import resize
 
 
+# Preprocess the atari game image to gray-scale 84*84 image
 def atari_img_preprocess(img):
     return resize(img_to_gray(img), (84, 84), mode='constant')
 #end
 
+# Modify the image into gray-scale image
 def img_to_gray(img):
     return np.dot(img, [0.299, 0.587, 0.114])
 #end
