67de2f3be1b193479073082c0d62a7332f3d1928
yunik1004@gmail.com <inkyu@DESKTOP-INKYU.localdomain>
2018-02-08 13:58:34 +0900

    Fix image process error

--- src/LearnAtariBoxing/preprocessors.py
+++ src/LearnAtariBoxing/preprocessors.py
@@ -1,5 +1,6 @@
 # Installed modules
 import numpy as np
+from skimage.color import rgb2gray
 from skimage.transform import resize
 # User-defined modules
 from LearnAtariBoxing.config import *
@@ -7,12 +8,5 @@ from LearnAtariBoxing.config import *
 
 # Preprocess the atari game image to gray-scale 84*84 image
 def atari_img_preprocess(img):
-    return resize(img_to_gray(img), (PROCESSED_INPUT_WIDTH, PROCESSED_INPUT_HEIGHT), mode='constant')
-#end
-
-# Modify the image into gray-scale image and reduce the range from 0 to 1
-## The shape of the image should be M*N*3
-def img_to_gray(img):
-    img_grayscale = np.dot(img, [0.299, 0.587, 0.114])
-    return img_grayscale# / 255.0
+    return resize(rgb2gray(img), (PROCESSED_INPUT_WIDTH, PROCESSED_INPUT_HEIGHT), mode='constant')
 #end
